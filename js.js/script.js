bb26779546602ea4cdbca66a349f5a0d691ce189
louisemogensenn <eaa23lmog@students.eaaa.dk>
2024-12-16 00:48:57 +0100

    Debugging
    Meget JS er flyttet fra script.js til deres egne html-filer

--- js.js/script.js
+++ js.js/script.js
@@ -39,180 +39,183 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         });
     });
-});
 
     /*----------------------- SCRIPT FOR PROGRAMSIDEN ------------------------*/
 
-    const overskriftProgram = document.getElementById('overskrift-program');
+    // const overskriftProgram = document.getElementById('overskrift-program');
 
-    const overskriftFiltrer = document.getElementById('overskrift-filtrer');
+    // const overskriftFiltrer = document.getElementById('overskriftFiltrer');
 
-    const filtrerEfterBehov = document.getElementById('filtrerEfterBehov');
+    // const filtrerEfterBehov = document.getElementById('filtrerEfterBehov');
 
-    const program = document.getElementById('program');
+    // const program = document.getElementById('program');
 
-    overskriftFiltrer.addEventListener('click', () => {
-        program.style.display = 'none';
-        filtrerEfterBehov.style.display = 'block';
-    });
+    // overskriftFiltrer.addEventListener('click', () => {
+    //     program.style.display = 'none';
+    //     filtrerEfterBehov.style.display = 'block';
+    // });
 
-    overskriftProgram.addEventListener('click', () => {
-        filtrerEfterBehov.style.display = 'none';
-        program.style.display = 'block';
-    });
+    // overskriftProgram.addEventListener('click', () => {
+    //     filtrerEfterBehov.style.display = 'none';
+    //     program.style.display = 'block';
+    // });
 
     /*----------------------- SCRIPT FOR FILTRERSIDEN ------------------------*/
 
-    const soegeikon = document.getElementById('soegeikon');
+    // const soegeikon = document.getElementById('soegeikon');
 
-    const popupSoegning = document.getElementById('popup-soegning');
+    // const popupSoegning = document.getElementById('popup-soegning');
 
-    const lukSoegningPopup = document.getElementById('lukSoegningPopUp');
+    // const lukSoegningPopup = document.getElementById('lukSoegningPopUp');
 
-    const soegeFelt = document.getElementById('soegefelt');
+    // const soegeFelt = document.getElementById('soegefelt');
 
-    const indholdPopupProgram = document.getElementById('indholdPopupProgram');
+    // const indholdPopupProgram = document.getElementById('indholdPopupProgram');
 
-    const prisKnap = document.getElementById('prisKnap');
+    // const prisKnap = document.getElementById('prisKnap');
 
-    const carloId = document.getElementById('carloId');
+    // const carloId = document.getElementById('carloId');
 
-    // Denne funktion åbnes pop-uppen, når man trukker på luppen.
-    soegeikon.addEventListener('click', () => {
-        popupSoegning.style.display = 'block'; /* Når man klikker på søgeikonet vises popup-vinduet */
-        if(soegeFelt.value.trim() === '') {
-            indholdPopupProgram.innerHTML = "Angiv søgeord"; /* Hvis søgefeltet er tomt vil denne besked angives */
-        } else if (soegeFelt.value.trim() === 'Carlo' || soegeFelt.value.trim() === 'carlo') {
-            filtreredeKoncerter.innerHTML = carloId.outerHTML; /* Her skal Carlos koncert sættes ind */
-            angivSoegekriterierTekst.style.display = 'none';
-            popupSoegning.style.display = 'none';
-        }
-        else {
-            indholdPopupProgram.innerHTML = 'Din søgning "' + soegeFelt.value + '" gav ingen resultater'; /* Hvis søgeordet ikke matcher nogen resultater, vil denne besked komme. Der er ikke lavet funktionalitet til at søge websitets indhold igennem. */
-            soegeFelt.value = ''; /* Inholdet i søgefeltet fjernes */
-        }        
-    });
+    // // Denne funktion åbnes pop-uppen, når man trukker på luppen.
+    // soegeikon.addEventListener('click', () => {
+    //     popupSoegning.style.display = 'block'; /* Når man klikker på søgeikonet vises popup-vinduet */
+    //     if(soegeFelt.value.trim() === '') {
+    //         indholdPopupProgram.innerHTML = "Angiv søgeord"; /* Hvis søgefeltet er tomt vil denne besked angives */
+    //     } else if (soegeFelt.value.trim() === 'Carlo' || soegeFelt.value.trim() === 'carlo') {
+    //         filtreredeKoncerter.innerHTML = carloId.outerHTML; /* Her skal Carlos koncert sættes ind */
+    //         angivSoegekriterierTekst.style.display = 'none';
+    //         popupSoegning.style.display = 'none';
+    //     }
+    //     else {
+    //         indholdPopupProgram.innerHTML = 'Din søgning "' + soegeFelt.value + '" gav ingen resultater'; /* Hvis søgeordet ikke matcher nogen resultater, vil denne besked komme. Der er ikke lavet funktionalitet til at søge websitets indhold igennem. */
+    //         soegeFelt.value = ''; /* Inholdet i søgefeltet fjernes */
+    //     }        
+    // });
 
-    // Luk popup, når man trykker på luk-knappen
-    lukSoegningPopup.addEventListener('click', () => {
-        popupSoegning.style.display = 'none';
-    });
+    // // Luk popup, når man trykker på luk-knappen
+    // lukSoegningPopup.addEventListener('click', () => {
+    //     popupSoegning.style.display = 'none';
+    // });
 
-    /* Nedstående funktion ændrer knappens farve ved at tjekke, hvilken farve den har først */
-    prisKnap.addEventListener('click', () => {
-        if (prisKnap.style.backgroundColor === 'var(--sekundaerKnapfarve)') {
-            prisKnap.style.backgroundColor = 'var(--knapfarve)'
-            prisKnap.style.border = 'none';
+    // /* Nedstående funktion ændrer knappens farve ved at tjekke, hvilken farve den har først */
+    // prisKnap.addEventListener('click', () => {
+    //     if (prisKnap.style.backgroundColor === 'var(--sekundaerKnapfarve)') {
+    //         prisKnap.style.backgroundColor = 'var(--knapfarve)'
+    //         prisKnap.style.border = 'none';
 
-        } else {
-            prisKnap.style.backgroundColor = 'var(--sekundaerKnapfarve)'
-            prisKnap.style.border = '1px solid var(--sekundaerFarve)';
-        }
+    //     } else {
+    //         prisKnap.style.backgroundColor = 'var(--sekundaerKnapfarve)'
+    //         prisKnap.style.border = '1px solid var(--sekundaerFarve)';
+    //     }
         
-    });
+    // });
 
-    /*drop down af priser */
+    // /*drop down af priser */
 
-    const dropdownPris = document.getElementById('dropdownPris');
+    // const dropdownPris = document.getElementById('dropdownPris');
 
-    const prisMuligheder = document.querySelectorAll('.dropdownPrisMulighed');
+    // const prisMuligheder = document.querySelectorAll('.dropdownPrisMulighed');
 
-    const nulstilFiltrer = document.getElementById('nulstilFiltrer');
+    // const nulstilFiltrer = document.getElementById('nulstilFiltrer');
 
-    const filtreredeKoncerter = document.getElementById('filtreredeKoncerter');
+    // const filtreredeKoncerter = document.getElementById('filtreredeKoncerter');
 
-    const gratisKoncerter = document.querySelectorAll('.gratisKoncert');
+    // const gratisKoncerter = document.querySelectorAll('.gratisKoncert');
 
-    const filtreringsmuligheder = document.getElementById('filtreringsmuligheder');
+    // const filtreringsmuligheder = document.getElementById('filtreringsmuligheder');
 
-    prisKnap.addEventListener('click', () => {
-        if(dropdownPris.style.display === 'block') {
-            dropdownPris.style.display = 'none';
-        } else {
-            dropdownPris.style.display = 'block';
-        }
-    });
+    // prisKnap.addEventListener('click', () => {
+    //     if(dropdownPris.style.display === 'block') {
+    //         dropdownPris.style.display = 'none';
+    //     } else {
+    //         dropdownPris.style.display = 'block';
+    //     }
+    // });
 
-    prisMuligheder.forEach(function (mulighed) {
-        mulighed.addEventListener('click', function () {
-            if (mulighed.innerHTML === 'GRATIS') {
-                prisKnap.innerHTML = 'GRATIS'; // Opdater knapteksten
-                dropdownPris.style.display = 'none'; // Skjul dropdownen
-                nulstilFiltrer.style.display = 'block'; // Vis "nulstil"-knappen
-                angivSoegekriterierTekst.style.display = 'none'; /* Teksten om at angive søgekriterier ønskes ikke vist, når der er angivet søgekriterier */
-                filtreredeKoncerter.innerHTML = gratisKoncerter.outerHTML; /* */
-                filtreredeKoncerter.style.justifyContent = 'center';
-                filtreredeKoncerter.style.margin = '0 300px 200px';
+    // prisMuligheder.forEach(function (mulighed) {
+    //     mulighed.addEventListener('click', function () {
+    //         if (mulighed.innerHTML === 'GRATIS') {
+    //             prisKnap.innerHTML = 'GRATIS'; // Opdater knapteksten
+    //             dropdownPris.style.display = 'none'; // Skjul dropdownen
+    //             nulstilFiltrer.style.display = 'block'; // Vis "nulstil"-knappen
+    //             angivSoegekriterierTekst.style.display = 'none'; /* Teksten om at angive søgekriterier ønskes ikke vist, når der er angivet søgekriterier */
+    //             filtreredeKoncerter.innerHTML = gratisKoncerter.outerHTML; /* */
+    //             filtreredeKoncerter.style.justifyContent = 'center';
+    //             filtreredeKoncerter.style.margin = '0 300px 200px';
                 
-                // Tøm tidligere viste koncerter
-                filtreredeKoncerter.innerHTML = '';
+    //             // Tøm tidligere viste koncerter
+    //             filtreredeKoncerter.innerHTML = '';
     
-                // Gå gennem alle gratis koncerter og vis dem
-                for (let i = 0; i < gratisKoncerter.length; i++) {
-                    filtreredeKoncerter.innerHTML += gratisKoncerter[i].outerHTML;
-                }
-            }
-        });
-    });
+    //             // Gå gennem alle gratis koncerter og vis dem
+    //             for (let i = 0; i < gratisKoncerter.length; i++) {
+    //                 filtreredeKoncerter.innerHTML += gratisKoncerter[i].outerHTML;
+    //             }
+    //         }
+    //     });
+    // });
 
-    nulstilFiltrer.addEventListener('click', () => {
-        prisKnap.innerHTML = 'PRIS';
-        prisKnap.style.backgroundColor = 'var(--knapfarve)';
-        nulstilFiltrer.style.display = 'none';
-        prisKnap.style.border = 'none';
-        filtreredeKoncerter.innerHTML = '';
-        angivSoegekriterierTekst.style.display = 'block';
-    });
-
-    /* HUSK AT FJERNE INDHOLDET, DER VISES, HVIS MAN KLIKKER NULSTIL */
+    // nulstilFiltrer.addEventListener('click', () => {
+    //     prisKnap.innerHTML = 'PRIS';
+    //     prisKnap.style.backgroundColor = 'var(--knapfarve)';
+    //     nulstilFiltrer.style.display = 'none';
+    //     prisKnap.style.border = 'none';
+    //     filtreredeKoncerter.innerHTML = '';
+    //     angivSoegekriterierTekst.style.display = 'block';
+    // });
     
 
     /*----------------------- SCRIPT FOR OM KUNSTNER  ------------------------*/
-    const reserverBilletKnap = document.getElementById('reserverBilletKnap');
 
-    const popupReserverBillet = document.getElementById('popup-reserverBillet');
 
-    reserverBilletKnap.addEventListener('click', () => {
-        popupReserverBillet.style.display = 'block';
-        setTimeout(() => { window.location.href = '404.html'; }, 2000);
-    });
+    // const reserverBilletKnap = document.getElementById('reserverBilletKnap');
+
+    // const popupReserverBillet = document.getElementById('popup-reserverBillet');
+
+    // console.log(popupReserverBillet);
+
+
+    // reserverBilletKnap.addEventListener('click', () => {
+    //     popupReserverBillet.style.display = 'block';
+    //     setTimeout(() => { window.location.href = '404.html'; }, 2000);
+    // });
 
     /*----------------------- SCRIPT FOR REPORTAGESIDEN - FRA FESTIVAL  ------------------------*/
 
-    const fraFestivalen = document.getElementById('fraFestivalen');
+    // const fraFestivalen = document.getElementById('fraFestivalen');
 
-    const fraArkivet = document.getElementById('fraArkivet');
+    // const fraArkivet = document.getElementById('fraArkivet');
 
-    const overskriftFestivalen = document.getElementById('overskriftFestivalen');
+    // const overskriftFestivalen = document.getElementById('overskriftFestivalen');
 
-    const overskriftArkivet = document.getElementById('overskriftArkivet');
+    // const overskriftArkivet = document.getElementById('overskriftArkivet');
 
-    overskriftArkivet.addEventListener('click', () => {
-        fraFestivalen.style.display = 'none';
-        fraArkivet.style.display = 'block';
-    });
+    // overskriftArkivet.addEventListener('click', () => {
+    //     fraFestivalen.style.display = 'none';
+    //     fraArkivet.style.display = 'block';
+    // });
 
-    overskriftFestivalen.addEventListener('click', () => {
-        fraArkivet.style.display = 'none';
-        fraFestivalen.style.display = 'block';
-    });
+    // overskriftFestivalen.addEventListener('click', () => {
+    //     fraArkivet.style.display = 'none';
+    //     fraFestivalen.style.display = 'block';
+    // });
 
     /*----------------------- SCRIPT FOR REPORTAGESIDEN - FRA ARKIV  ------------------------*/
-    const skiftPlakatSideFrem = document.getElementById('skiftPlakatsideFrem');
+    // const skiftPlakatSideFrem = document.getElementById('skiftPlakatsideFrem');
 
-    const skiftPlakatsideTilbage = document.getElementById('skiftPlakatsideTilbage');
+    // const skiftPlakatsideTilbage = document.getElementById('skiftPlakatsideTilbage');
 
-    const foerstePlakatSide = document.getElementById('foerstePlakatSide');
+    // const foerstePlakatSide = document.getElementById('foerstePlakatSide');
 
-    const andenPlakatSide = document.getElementById('andenPlakatSide');
+    // const andenPlakatSide = document.getElementById('andenPlakatSide');
 
-    skiftPlakatSideFrem.addEventListener('click', () => {
-        foerstePlakatSide.style.display = 'none';
-        andenPlakatSide.style.display = 'block';
-    });
+    // skiftPlakatSideFrem.addEventListener('click', () => {
+    //     foerstePlakatSide.style.display = 'none';
+    //     andenPlakatSide.style.display = 'block';
+    // });
 
-    skiftPlakatsideTilbage.addEventListener('click', () => {
-        andenPlakatSide.style.display = 'none';
-        foerstePlakatSide.style.display = 'block';
-    });
+    // skiftPlakatsideTilbage.addEventListener('click', () => {
+    //     andenPlakatSide.style.display = 'none';
+    //     foerstePlakatSide.style.display = 'block';
+    // });
 
+});
