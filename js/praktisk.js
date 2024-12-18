34cde016defe5e42c5eaa986577a91b72b79f2db
Frederik <eaa24fdh@students.eaaa.dk>
2024-12-18 11:51:08 +0100

    css rettelser

--- js/praktisk.js
+++ js/praktisk.js
@@ -105,7 +105,7 @@ if (spoergsmaal === "Hvad er parkeringsmulighederne?") {
     // Tilføjer klik-hændelse til knappen
     viderstilKnap.addEventListener('click', () => {
         // Åbner et nyt tomt vindue (eller specifik side)
-        window.open('404.html', '_blank'); /* Vi har ikke lavet et parkeringskort? */
+        window.open('404.html', '_blank'); /* Vi har ikke lavet et parkeringskort */
     });
     
     // Tilføjer knappen til svar-elementet
